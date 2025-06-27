bc84e511970639ed49718ae6ccc49311d4a72430
Nidhi Choudhari <153424851+nidhichoudhari@users.noreply.github.com>
2025-06-27 23:03:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (87.91%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            if (((nums[i] ^ nums[i-1]) & 1) == 0)  // Check if XOR's last bit is 0
-                return false;  // \U0001f6ab Same parity → invalid
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] % 2 == nums[i + 1] % 2) {
+                return false;
+            }
         }
-        return true;  // ✅ Valid special array
+        return true;
     }
 }
