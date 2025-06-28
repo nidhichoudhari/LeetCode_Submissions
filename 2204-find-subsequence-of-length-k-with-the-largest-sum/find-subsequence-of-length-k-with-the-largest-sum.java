39ffad9bb4a5e9f54080c163e9c6c3bb8932a344
Nidhi Choudhari <153424851+nidhichoudhari@users.noreply.github.com>
2025-06-28 16:02:58 +0530

    Time: 8 ms (59.71%) | Memory: 44.9 MB (27.82%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.java
@@ -2,16 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] maxSubsequence(int[] nums, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
-        for (int i = 0; i < nums.length; i++) {
-            pq.offer(new int[]{nums[i], i});
-            if (pq.size() > k) pq.poll();
+        int n = nums.length;
+        int[][] indexed = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            indexed[i][0] = nums[i];
+            indexed[i][1] = i;
         }
-        int[][] indices = pq.toArray(new int[0][0]);
-        Arrays.sort(indices, Comparator.comparingInt(a -> a[1]));
+
+        Arrays.sort(indexed, (a, b) -> b[0] - a[0]); // sort by value descending
+        int[][] topK = Arrays.copyOfRange(indexed, 0, k);
+
+        Arrays.sort(topK, Comparator.comparingInt(a -> a[1])); // sort by original index
+
         int[] result = new int[k];
         for (int i = 0; i < k; i++) {
-            result[i] = nums[indices[i][1]];
+            result[i] = topK[i][0];
         }
         return result;
     }
