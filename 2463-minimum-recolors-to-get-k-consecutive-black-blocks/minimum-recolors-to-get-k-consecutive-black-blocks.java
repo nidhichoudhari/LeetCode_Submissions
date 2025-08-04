0b56f0fdb5d8f7e7ac53cb1d66b63669ebcf46fb
Nidhi Choudhari <153424851+nidhichoudhari@users.noreply.github.com>
2025-08-04 21:28:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (73.55%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,32 +1,22 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int n=blocks.length();
-        int wCnt=0;
-
-        // Count white blocks in the initial window of size k
-        for(int l=0; l<k; l++){
-            if(blocks.charAt(l)=='W'){
-                wCnt++;
-            }
+        int n = blocks.length();
+        int w = 0;
+        
+        // Initial count of 'W' in the first window
+        for (int i = 0; i < k; i++) {
+            if (blocks.charAt(i) == 'W') w++;
         }
         
-        int i=1, j=k; //Two Pointers
-        int cnt=wCnt;
-
-        // Slide the window through the string
-        while(i<n && j<n){       
-            // Subtract the "W" block leaving the window     
-            if(blocks.charAt(i-1)=='W'){
-                wCnt-=1;
-            }
-            // Add the "W" block entering the window
-            if(blocks.charAt(j)=='W'){
-                wCnt+=1;
-            }
-            cnt=wCnt<cnt?wCnt:cnt;
-            i++; j++;
-        }      
-
-        return cnt;
+        int ans = w;
+        
+        // Sliding Window
+        for (int i = k; i < n; i++) {
+            if (blocks.charAt(i) == 'W') w++;
+            if (blocks.charAt(i - k) == 'W') w--;
+            ans = Math.min(ans, w);
+        }
+        
+        return ans;
     }
 }
